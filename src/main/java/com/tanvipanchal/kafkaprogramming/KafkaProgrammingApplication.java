127c03975fe84448109dd5c8a247c036f164ef53
Tanvi <tanvipanchal2610@gmail.com>
2023-11-14 10:28:50 -0800

    kafka basic app with producer code

---
+++ src/main/java/com/tanvipanchal/kafkaprogramming/KafkaProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.tanvipanchal.kafkaprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanvipanchal/kafkaprogramming/KafkaProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvipanchal.kafkaprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
